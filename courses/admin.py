71134914917163712aa482bafd7b7ba0ccb2f29f
ctalavero <ctalavero12@gmail.com>
2024-05-14 15:31:27 +0300

    add course models and their admin settings

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,81 @@
 from django.contrib import admin
+from .models import Course, Module, Lesson, Content, Text, File, Image, Video
 
-# Register your models here.
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+    extra = 1
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 1
+
+class ContentInline(admin.StackedInline):
+    model = Content
+    extra = 1
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by', 'created_at', 'rating')
+    list_filter = ('created_at', 'tags')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    filter_horizontal = ('user',)
+    readonly_fields = ('created_at',)
+    fieldsets = (
+        (None, {
+            'fields': ('created_by', 'title', 'slug', 'description', 'tags', 'user', 'passage_time', 'rating')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at',),
+        }),
+    )
+    inlines = [ModuleInline]
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course', 'order')
+    list_filter = ('course',)
+    search_fields = ('title', 'description')
+    inlines = [LessonInline]
+
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'module', 'order', 'passage_time')
+    list_filter = ('module',)
+    search_fields = ('title',)
+    inlines = [ContentInline]
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('lesson', 'content_type', 'object_id', 'order')
+    list_filter = ('lesson', 'content_type')
+    search_fields = ('lesson__title',)
+
+@admin.register(Text)
+class TextAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    readonly_fields = ('created_at', 'updated_at')
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at', 'file')
+    search_fields = ('title',)
+    readonly_fields = ('created_at', 'updated_at')
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at', 'file')
+    search_fields = ('title',)
+    readonly_fields = ('created_at', 'updated_at')
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at', 'url', 'file')
+    search_fields = ('title', 'url')
+    readonly_fields = ('created_at', 'updated_at')
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,95 @@
+from django.core.exceptions import ValidationError
 from django.db import models
-
+from taggit.managers import TaggableManager
+from .fields import OrderField
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
+
+class Course(models.Model):
+    created_by = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='courses_created')
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    tags = TaggableManager()
+    user = models.ManyToManyField('auth.User', related_name='courses_joined', blank=True)
+    passage_time = models.DurationField(blank=True, null=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=2,blank=True, null=True)
+
+    def clean_rating(self):
+        if self.rating < 0 or self.rating > 5:
+            raise ValidationError('Rating must be between 0 and 5')
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f'{self.order}). {self.title}'
+
+
+
+class ItemABS(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+class Text(ItemABS):
+    content = models.TextField()
+
+class File(ItemABS):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemABS):
+    file = models.FileField(upload_to='images')
+
+class Video(ItemABS):
+    url = models.URLField(blank=True, null=True)
+    file = models.FileField(upload_to='videos', blank=True, null=True)
+
+    def clean(self):
+        if not self.url and not self.file:
+            raise ValidationError('Either URL or file must be provided.')
+
+class Lesson(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    order = OrderField(blank=True, for_fields=['module'])
+    passage_time = models.DurationField(blank=True, null=True)
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f'{self.order}). {self.title}'
+
+class Content(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+    order = OrderField(blank=True, for_fields=['lesson'])
+
+    class Meta:
+        ordering = ['order']
+
