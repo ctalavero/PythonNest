5955cc23dc39379275bb4a557db615b5a2d05615
ctalavero <ctalavero12@gmail.com>
2024-05-20 14:36:30 +0300

    add docks

--- courses/functions.py
+++ courses/functions.py
@@ -2,14 +2,35 @@ import requests
 import  isodate
 from urllib.parse import urlparse, parse_qs
 from moviepy.editor import VideoFileClip
-def get_video_id_from_url(url):
+from typing import Optional
+
+def get_video_id_from_url(url: str) -> Optional[str]:
+    """
+    Extracts the video ID from a YouTube URL.
+
+    Parameters:
+    url (str): The YouTube URL.
+
+    Returns:
+    str: The video ID, or None if the URL does not contain a video ID.
+    """
     parsed_url = urlparse(url)
     video_id = parse_qs(parsed_url.query).get('v')
     if video_id:
         return video_id[0]
     return None
 
-def get_youtube_video_duration(video_id, api_key):
+def get_youtube_video_duration(video_id: str, api_key: str) -> float:
+    """
+    Retrieves the duration of a YouTube video in seconds.
+
+    Parameters:
+    video_id (str): The ID of the YouTube video.
+    api_key (str): The API key to use for the YouTube Data API v3.
+
+    Returns:
+    float: The duration of the video in seconds.
+    """
     url = f"https://www.googleapis.com/youtube/v3/videos?id={video_id}&part=contentDetails&key={api_key}"
     response = requests.get(url)
     data = response.json()
@@ -17,6 +38,15 @@ def get_youtube_video_duration(video_id, api_key):
     return duration.total_seconds()
 
 def get_video_duration(video_path):
+    """
+    Retrieves the duration of a video file in seconds.
+
+    Parameters:
+    video_path (str): The path to the video file.
+
+    Returns:
+    float: The duration of the video in seconds.
+    """
     clip = VideoFileClip(video_path)
     duration = clip.duration # in seconds
     return duration
